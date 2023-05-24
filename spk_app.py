e93963a4460bc3bdac8146879b4b3dc116dd9028
Patrick Hayes <pfhayes@umich.edu>
2023-05-24 16:25:52 -0400

    expander

--- spk_app.py
+++ spk_app.py
@@ -10,21 +10,18 @@ import streamlit as st
 st.set_page_config(
     page_title="Probable Starting Pitcher Strikeouts",
     # page_icon="🧊",
-    # layout="wide",
-    initial_sidebar_state="expanded",
+    layout="wide",
+    # initial_sidebar_state="expanded",
     menu_items={
-        # 'Get Help': 'https://www.extremelycoolapp.com/help',
-        # 'Report a bug': "https://www.extremelycoolapp.com/bug",
         'About': "# SP Strikeouts!"
     }
 )
-
-# test
 
 tab1, tab2, tab3, tab4 = st.tabs(["Expected Ks", "xK Distribution", "Over/Under Bets", "Most Ks Odds"])
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
+#imports
 df = pd.read_csv('spk_viz_data.csv')
 sp_df = df[['Name', 'Team', 'Handedness', 'Opponent', 'xK', 'prop_k']]
 df_under = pd.read_csv('spk_under_today.csv')
@@ -50,7 +47,7 @@ for col in under_odds:
 
 df_over = df_over[['Name', 'Team', 'Opponent', 'xK', 'prop_k', 'over', 'expected value',  'over_odds', 'x_over']]
 df_under = df_under[['Name', 'Team', 'Opponent', 'xK', 'prop_k', 'under', 'expected value', 'under_odds', 'x_under']]
-
+#sim updates
 df_spk_sim['Percent'] = df_spk_sim['Percent'].apply(to_percent)
 df_spk_sim.drop(columns=['Count'], inplace=True)
 
@@ -103,22 +100,52 @@ def plot_strikeout_distributions(df, fig_width=8, fig_height=10, n_cols=2):
     plt.show()
 
 
+
+
+
+
+
 def app():
+    # with st.sidebar:
+    #     st.write('test')
+    #### mark down test here
+    tab1kd = """
+    #### Objective:
+    to showcase data science skills in creating a sports analytics app
+    #### Method:
+    - scrape SP probables from two sites (Rotogrinders and BaseballPress)
+    - scrape team batting stats from Baseball Reference via pybaseball package
+    - scrape pitcher stats from Baseball Reference via pybaseball package
+    - calculate expected strikeout distributions for each pitcher
+    - use API to pull over / under prop bets from FanDuel
+    - calculate expected value for each pitcher
+    - create a streamlit app to display the data
+    """
+
     with tab1:
         st.header("Expected Strikeouts (xK)")
         st.text(f"For games played on {today}")
-        st.text('The expected number of strikeouts is calculated using pitcher stats and opponent team batting stats. A further explanation is coming.')
+        # with st.expander('Why I Made This (Click to Expand)'):
+        #     st.write('Juicy deets')
+        #     st.markdown(tab1kd)
+        
+        with st.expander('Glossary & Methodology (Click to Expand)'):
+            st.markdown("""
+            - **xK**: expected number of strikeouts
+            - **prop_k**: FanDuel over / under prop bet for strikeouts
+            """)
+            st.write('The expected number of strikeouts is calculated using pitcher stats and opponent team batting stats. A further explanation is coming.')
         st.dataframe(sp_df, height=700, width=1000)
     with tab2:
         st.header("Expected Strikeout Distributions")
         st.text(f"For games played on {today}")
-        st.text('A Poisson distribution plot of the expected number of strikeouts per pitcher')
+        st.write('A Poisson distribution plot of the expected number of strikeouts per pitcher')
         fig = plot_strikeout_distributions(df)
         st.pyplot(fig)
 
     with tab3:
         st.text(f"For games played on {today}")
-        st.text('expected value is the difference between the expected percent likelihood vs. the prop bet percent likelihood')
+        st.write('expected value is the difference between the expected percent likelihood vs. the prop bet percent likelihood')
         st.subheader("Under Prop Bets")
         st.dataframe(df_under)
         st.subheader("Over Prop Bets")
@@ -126,8 +153,8 @@ def app():
     with tab4:
         st.header("Most Ks Odds")
         st.text(f"For games played on {today}")
-        st.text('The odds below are based on 10,000 simulations of the games being played today.')
-        st.text('Moneyline to be fixed for lines that should be negative.')
+        st.write('The odds below are based on 10,000 simulations of the games being played today.')
+        st.write('Notice: Moneyline to be fixed for lines that should be negative.')
         st.dataframe(df_spk_sim)
 
 
