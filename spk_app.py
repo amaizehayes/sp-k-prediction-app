9774c8736147247f46916fd298e009a23205c903
Patrick Hayes <pfhayes@umich.edu>
2023-05-25 09:55:14 -0400

    about update

--- spk_app.py
+++ spk_app.py
@@ -13,7 +13,7 @@ st.set_page_config(
     layout="wide",
     # initial_sidebar_state="expanded",
     menu_items={
-        'About': "# SP Strikeouts!"
+        'About': "# SP Strikeouts! More info to come."
     }
 )
 
