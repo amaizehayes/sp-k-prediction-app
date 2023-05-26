ba2e3d523b5156c3bc2be4a108154e73ef802e14
Patrick Hayes <pfhayes@umich.edu>
2023-05-26 15:24:48 -0400

    bug info

--- pages/about.py
+++ pages/about.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="SP K About",
     page_icon="⚾",
-    layout="wide",
+    # layout="wide",
     initial_sidebar_state="auto",
     menu_items={
         'About': """# Created by Patrick Hayes
