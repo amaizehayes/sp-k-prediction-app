6b6cc1a5922fef23d74be133283eed6fc552e080
Patrick Hayes <pfhayes@umich.edu>
2023-05-30 23:21:50 -0400

    run location update

--- run_scripts.sh
+++ run_scripts.sh
@@ -5,5 +5,5 @@ python scripts/spk_logs_scrape.py #run once
 python scripts/spk_odds_api.py #run multiple times, historic element to figure out
 python scripts/spk_output.py #run multiple times
 python scripts/spk_sim.py #run multiple times
-python scripts/spk_actuals.py
-python spk_push.py
+python scripts/spk_actuals.py #run once
+python scripts/spk_push.py #run when pushing
