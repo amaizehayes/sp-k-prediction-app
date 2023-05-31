be61b294a30a571c67348d2f4e04c3ff081bcc91
Patrick Hayes <pfhayes@umich.edu>
2023-05-30 23:03:39 -0400

    json inclusion

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-python scripts/spk_probables.py
-python scripts/team_k_splits.py
-python scripts/spk_logs_scrape.py
-python scripts/spk_odds_api.py
-python scripts/spk_output.py
-python scripts/spk_sim.py
+python scripts/spk_probables.py #run multiples times
+python scripts/team_k_splits.py #run once
+python scripts/spk_logs_scrape.py #run once
+python scripts/spk_odds_api.py #run multiple times, historic element to figure out
+python scripts/spk_output.py #run multiple times
+python scripts/spk_sim.py #run multiple times
 python scripts/spk_actuals.py
 python spk_push.py
