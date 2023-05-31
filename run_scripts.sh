276ddd8bea1c2f644627b119bd033e26675ac146
Patrick Hayes <pfhayes@umich.edu>
2023-05-30 22:51:14 -0400

    bash folder edits

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 python scripts/spk_probables.py
-python team_k_splits.py
-python spk_logs_scrape.py
-python spk_odds_api.py
-python spk_output.py
-python spk_sim.py
-python spk_actuals.py
+python scripts/team_k_splits.py
+python scripts/spk_logs_scrape.py
+python scripts/spk_odds_api.py
+python scripts/spk_output.py
+python scripts/spk_sim.py
+python scripts/spk_actuals.py
 python spk_push.py
